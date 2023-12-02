a759b4ee22b16ff60cf08d22467eaf636da8a5e9
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2023-12-02 16:31:43 -0500

    [add] - Basic routes

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,7 +24,7 @@ const appMenuItems = <MenuItem>[
   MenuItem(
     title: 'Cards',
     subTitle: 'Un contenedor estilizado',
-    link: '/buttons',
+    link: '/cards',
     icon: Icons.credit_card,
   ),
 ];
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -48,6 +49,11 @@ class _CustomLisTitle extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       onTap: () {
         // TODO - Navigate
+
+        // Navigator.of(context).push(MaterialPageRoute(
+        //   builder: (context) => const ButtonsScreen(),
+        // ));
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
